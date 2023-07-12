afef3a973864bbcd3588f28a7141e1d4866f81e2
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-12 03:05:25 -0700

    Addition - 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 10-add.py
+
+def add(a, b):
+    """Return the additio  of a amd b"""
+    return (a+b)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # 8-uppercase.py
 
-def print_last_digit(number):
-    """print the last digit of a number and return it."""
-    print(abs(number) % 10, end="")
-    return(abs(number) % 10)
+def uppercase(str):
+    """prints a string uppercase"""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+            print"{}".format(c), end="")
+            print("")
