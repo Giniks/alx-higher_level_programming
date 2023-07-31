1388fcebb2312bfef5a79d72b70c85b3037ea76c
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-31 06:19:12 -0700

    index error exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """print x element of a list.
+
+    Args:
+        my_list (list): The list where the element will be printed from.
+        x (int): The number of elements to print.
+
+    Returns:
+        The real number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+        print("")
+        return (ret)
