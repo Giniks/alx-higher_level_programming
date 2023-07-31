004dcec2518f0f5d2f526e564bdee5bf6dad49f1
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-31 10:28:24 -0700

    prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """prints an integer value.
+
+    Args:
+        value (int); The value integer to be printed.
+
+    Returns:
+        True if the value integer has been correctly printed.
+        otherwise, false and prints with stderr preceded by Exception.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
