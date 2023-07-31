f7ef968e13958ffddd836a38591031ca365113fe
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-31 07:13:30 -0700

    print integer value

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/python3
+
+ def safe_print_integer(value):
+    """print an integer with "{:d}".format().
+
+    Args:
+        value (int): The inter value to be printed.
+
+    Returns:
+        True if value printed is integer.
+        Otherwise it returns false.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except IndexError:
+        break
+    print("")
+    return (ret)
