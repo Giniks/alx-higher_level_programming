0d601db5c645ad8c67716b9f075aaeeb6f14db29
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-08-01 10:05:10 -0700

    square size exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
