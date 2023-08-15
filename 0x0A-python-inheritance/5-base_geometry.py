c52be473586323ae09fc1400c3903772c2a0f4f5
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-08-14 20:02:50 -0700

    base

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines an empyy class - BaseGeometry"""
 
- class  BaseGeometry:
-     """Represents the essence of BaseGeometry"""
-     pass
 
+class BaseGeometry:
+    """Represents the essence of BaseGeometry"""
+    pass
