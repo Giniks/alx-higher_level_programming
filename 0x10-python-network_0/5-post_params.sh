b6dc7acb58bd7b6fb7285449643a436d62cbb945
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-11-02 18:02:09 -0700

    peak

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST to URL with custom variables & displays the body of the response
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
