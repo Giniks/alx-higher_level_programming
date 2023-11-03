8219674727e44d2aac6ed780718840af1513cc85
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-11-02 18:39:29 -0700

    json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bssh
 # srnds JSON POST request and displays the body of response
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1" | sed 'N;$!P;D'
+curl -sLX POST -H "Content-Type: application/json" -d @"$2" "$1"
