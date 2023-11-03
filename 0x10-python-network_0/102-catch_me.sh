5583db3e16e1811fca08bb41cefa5db35f126b94
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-11-02 19:06:29 -0700

    catchme

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to the server to get the response containing "You got me!"
-curl -sLX PUT -d 'user_id=98' "You got me!" -H 'Origin:School' "0.0.0.0:5000/catch_me"
+# Post Request
+curl -sLX PUT -d 'user_id=98' -H 'Origin:School' "0.0.0.0:5000/catch_me"
