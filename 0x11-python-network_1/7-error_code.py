da1d6779eaa2b25fa5c2ad2c20cc9ebd0ab17ed0
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-11-03 18:16:59 -0700

    update

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-This is a script that fetches url, Send request and displays the body response.
-print: Error code: if the  HTTP status code is greater than or equal to 400.
-Followed by the HTTP status codr.
+This is a script that fetches url, Send request and displays the body response
+print: Error code: if the  HTTP status code is greater than or equal to 400
+Followed by the HTTP status code
 """
 
 
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = request.get(url)
+    r = requests.get(url)
     if r.status_code >= 400:
-        print(f"Error code: {r.status_code}")
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
-
